f18bffc434e72d0aeea8147eb320e6ddb02bd413
Kimberly Arenas <karenas@miners.utep.edu>
2021-03-01 17:13:38 -0700

    working on tokenize, print and free

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -60,19 +60,59 @@ int count_words(char *str){
   return count; 
 
 }
-char *copyStr(char *inStr, short len){
+char *copy_str(char *inStr, short len){
   
-char *cStr = (char*)malloc(sizeof(char)*(len+1));
- int i = 0;
- while(i<len){
-   cStr[i] = inStr[i];
-   i++;
- }
- cStr[len] = '\0';
- return cStr;
+  char *cStr = (char*)malloc(sizeof(char)*(len+1));
+  int i = 0;
+  while(i<len){
+    cStr[i] = inStr[i];
+    i++;
+  }
+  cStr[len] = '\0';
+  return cStr;
 }
 
+char **tokenize(char* str){
+  int length = count_words(str); //stores the number of words in the string 
+  char **char_array;
+  char_array = (char**)malloc(sizeof(char*)*(length +1)); //array memory allocate
+
+  int i = 0;
+  while(i < length){
+    str = word_start(str);
+    int stringLen = word_terminator(str) - str; //calculates the length of the current string 
+    char_array[i] = copy_str(str, stringLen);
+    str = word_terminator(str); //sets str to begin after word terminates  
+    i++;
+  }
+  char_array[i] = NULL;
+  return char_array;
+ }
+
+void print_tokens(char **tokens){
+ /*
+ while(*tokens != NULL){ 
+   printf("<%s>", *tokens);
+   tokens++;
+
+ }
+ */
+ for(int i=0; tokens[i] != NULL;i++){
+   printf("%d",tokens[i]);
+ }
+}
+  
+void free_tokens(char **tokens){
+  int i =0;
+
+  while(*tokens != NULL){
+    free(*tokens);
+    tokens++;
+  }
+}
+  
   int main(){
+    /*
     char str[] = "  hello";
     char *ptr = word_start(str);
     printf("\n%p\n", ptr);
@@ -84,7 +124,17 @@ char *cStr = (char*)malloc(sizeof(char)*(len+1));
     printf("%p", &strend[5]);
 
     printf("\n%i\n", count_words("hello world"));
-
+    
     char strCopy[] = "hello there world";
-      printf("%s",copyStr(str,sizeof(str)));
+      printf("%s",copy_str(strCopy,5));
+    */
+    char tok[] = "hello there world";
+    print_tokens(tokenize(tok));
+      /*
+      char strT[] = "hello";
+      char strT2[] = "world";
+      char strT3[] = "hi";
+
+      char *tokens[] = {strT, strT2, strT3};
+      */
   }

312386f7545048105d6b94ca5b99e4f5e7daf684
Kimberly Arenas <karenas@miners.utep.edu>
2021-03-04 21:45:40 -0700

    remove tokenizer1 because unnecessary

---
+++ src/history.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "history.h"
+
+List *init_history(){
+
+  List *histList = malloc(sizeof(List));
+  histList->root = NULL;
+  return histList;
+}
+
+void add_history(List *list, char *str){
+  Item  *node = (Item*)malloc(sizeof(Item));
+
+  node-> str = str;
+  node-> next = NULL;
+  if(list->root == NULL){
+
+    list->root = node;
+
+    node-> id = 1;
+  }
+  else{
+    int count = 2;
+    Item *holder;
+    holder = list-> root;
+    while(holder->next != NULL){
+      count++;
+      holder = holder->next;
+    }
+    holder->next = node;
+    node->id = count;
+  }
+}
+
+char *get_history(List *list, int id){
+  if(id == 1){
+    list->root->str;
+  }
+  Item *holder = list->root;
+  while(holder != NULL){
+    if(holder ->id == id){
+      return holder->str;
+    }
+    holder = holder->next;
+  }
+  return NULL;
+}
+
+void print_history(List *list){
+  Item *holder = list->root;
+  while(holder != NULL){
+    printf("ID<%i> STR[%s]\n", holder->id, holder->str);
+    holder = holder->next;
+  }
+}
+
+void free_history(List *list){
+  free(list);
+}
